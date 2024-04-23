9da9a72b9d6ce488569ebde8cc5fe4651d38cf48
Andrew-Nzioki <andrewkyalonzioki001@gmail.com>
2024-04-23 19:28:41 +0300

    commit

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Run migrations
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+# Seed Database
+python3 manage.py loaddata menus.json
+python3 manage.py loaddata menu_items.json
+python3 manage.py loaddata testimonials.json
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Run the specified command (if any)
+exec "$@"
